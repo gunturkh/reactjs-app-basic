7238df3df91d1333d60311e9057466efb8e6bb5f
gunturkh <guntur.kh@gmail.com>
2018-08-16 21:25:45 +0700

    Create App folder

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from '../Logo/logo.svg'
+import './index.css'
 
 class App extends Component {
   
