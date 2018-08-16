32963f31a429dc0f928969e89a22c3b8a62d511b
gunturkh <guntur.kh@gmail.com>
2018-08-16 11:25:33 +0700

    Installing and configuring eslint plugin

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  handleClick(){
+    alert('Clicked')
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React App</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type="input" name="Name"/>
+        <button type="button" onClick={this.handleClick}> Send </button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
