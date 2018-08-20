9156cca102fec5dc5d4ae57041ce680350cf181c
gunturkh <guntur.kh@gmail.com>
2018-08-20 09:52:26 +0700

    Fix delete method for data from database

--- src/App/index.js
+++ src/App/index.js
@@ -11,26 +11,36 @@ class App extends Component {
     this.state = { 
       items: []
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
     this.addItem = this.addItem.bind(this)
     this.deleteItem = this.deleteItem.bind(this)
   }
+  
 
-  addItem(e) {
+  handleSubmit(e){
     if (this._inputElement.value !== '') {
       let newItem = {
         text: this._inputElement.value,
         key: Date.now()
       }
- 
-      this.setState((prevState) => {
-        return { 
-          items: prevState.items.concat(newItem) 
-        }
-      })
+      this.addItem(newItem)
       this._inputElement.value = ''
     }
     e.preventDefault()
   }
+
+  addItem(item) {
+    this.setState((prevState) => {
+      return { 
+        items: prevState.items.concat(item) 
+      }
+    })
+    
+    
+  }
+
+
+
   deleteItem(key) {
     let filteredItems =this.state.items.filter(function (item) {
       return (item.key !== key)
@@ -46,18 +56,19 @@ class App extends Component {
       text: event.target.value
     })
   }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title"></h1>
-          <form onSubmit={this.addItem}>
+          <form onSubmit={this.handleSubmit}>
             <input ref={(a) => this._inputElement = a} placeholder="Enter Task"></input>
             <button type="submit">Add</button>
           </form>
         </header>
-        <TodoItems entries = {this.state.items} delete = {this.deleteItem}/>
+        <TodoItems entries = {this.state.items} delete = {this.deleteItem} addItem = {this.addItem}/>
       </div>
     )
   }
--- src/TodoItems/index.js
+++ src/TodoItems/index.js
@@ -5,6 +5,22 @@ class TodoItems extends React.Component {
   constructor(props) {
     super(props)
     this.createTasks = this.createTasks.bind(this)
+    this.delete = this.delete.bind(this)
+  }
+
+  componentDidMount() {
+    fetch('https://learn-heroku-deploy.herokuapp.com/')
+      .then(results => {
+        return results.json()
+      })
+      .then (data => {
+        data.todo_lists.forEach( item => {
+          this.props.addItem({
+            text: item.todo_task,
+            key: item.id
+          })
+        })
+      })
   }
 
   delete (key) {
@@ -15,11 +31,10 @@ class TodoItems extends React.Component {
     return <li onClick={() => this.delete(item.key)} key = {item.key}>{item.text}</li>
   }
 
-
   render () {
     let todoEntries = this.props.entries
     let listItems = todoEntries.map(this.createTasks)
-
+    
     return (
       <ul className="theList">
         <FlipMove duration={250} easing="ease-out">
